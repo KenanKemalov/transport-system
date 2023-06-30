9635dffbc6d3d19ff7ba75c32321a2a85e10a830
Kenan <kenankemalow@gmail.com>
2023-06-30 21:30:58 +0300

    frontend - table data pagination

---
+++ src/main/resources/static/js/script.js
@@ -0,0 +1,25 @@
+// $('#example').DataTable();
+
+
+$(document).ready(function () {
+    $('#example').DataTable({
+        "paging": true,
+        "lengthChange": false,
+        "searching": false,
+        "ordering": true,
+        "info": true,
+        "autoWidth": false,
+        "pageLength": 5 // Set the number of items per page here
+    });
+    $('#example10').DataTable({
+        "paging": true,
+        "lengthChange": false,
+        "searching": false,
+        "ordering": true,
+        "info": true,
+        "autoWidth": false,
+        "pageLength": 10 // Set the number of items per page here
+    });
+    // $('#example').DataTable();
+    // $('.dataTables_length').addClass('bs-select');
+});
